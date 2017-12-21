5d0799ea57088e5200f3620ed755dc0c332fa888
Jason Carrick <carrick.jason@gmail.com>
2017-12-20 23:23:31 -0700

    adding netlify identity script back

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import Script form 'react-load-script'
 
 export const IndexPageTemplate = ({ data }) => (
   <pre>{JSON.stringify(data, null, 4)}</pre>
 )
 
 export default class IndexPage extends React.Component {
+  handleScriptLoad() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/'
+          })
+        }
+      })
+    }
+    window.netlifyIdentity.init()
+  }
+
   render() {
     const { data } = this.props
-    return <IndexPageTemplate data={data} />
+    return (
+      <section>
+        <Script
+          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          onLoad={this.handleScriptLoad.bind(this)}
+        />
+        <IndexPageTemplate data={data} />
+      </section>
   }
 }
 
