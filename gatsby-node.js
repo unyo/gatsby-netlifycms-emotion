12315d3c53a8ce2daf9cbfd236eb9d662b621850
Jason Carrick <carrick.jason@gmail.com>
2017-12-20 20:46:34 -0700

    Removing images, updating gatsby node and netlify config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,63 +1,23 @@
-const path = require('path');
+const path = require('path')
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPage } = boundActionCreators;
+  const { createPage } = boundActionCreators
 
   return graphql(`
     {
-      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
         edges {
           node {
-            excerpt(pruneLength: 400)
             html
             id
             frontmatter {
-              templateKey
               path
-              date
+              templateKey
               title
-              image
-              heading
-              description
-              intro {
-                blurbs {
-                  image
-                  text
-                }
-                heading
-                description
-              }
-              main {
-                heading
-                description
-                image1 {
-                  alt
-                  image
-                }
-                image2 {
-                  alt
-                  image
-                }
-                image3 {
-                  alt
-                  image
-                }
-              }
-              testimonials {
-                author
-                quote
-              }
-              full_image
-              pricing {
-                heading
-                description
-                plans {
-                  description
-                  items
-                  plan
-                  price
-                }
-              }
+              textSection
             }
           }
         }
@@ -65,15 +25,19 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     }
   `).then(result => {
     if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()));
-      return Promise.reject(result.errors);
+      result.errors.forEach(e => console.error(e.toString()))
+      return Promise.reject(result.errors)
     }
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      const compPath = node.frontmatter.templateKey
+        ? `src/templates/${String(node.frontmatter.templateKey)}.js`
+        : `src/pages/index.js`
+
       createPage({
         path: node.frontmatter.path,
-        component: path.resolve(`src/templates/${String(node.frontmatter.templateKey)}.js`),
+        component: path.resolve(compPath),
         context: {} // additional data can be passed via context
-      });
-    });
-  });
-};
+      })
+    })
+  })
+}
